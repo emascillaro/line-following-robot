b59b57797204011bc3d3ffcf845751ca60d350dd
Chidunbo <97328823+Chidunbo@users.noreply.github.com>
2022-10-06 16:33:43 -0400

    Add basic motor test

---
+++ test_motor/test_motor.ino
@@ -0,0 +1,20 @@
+#include <AFMotor.h>
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *Motor1 = AFMS.getMotor(1);
+Adafruit_DCMotor *Motor2 = AFMS.getMotor(2);
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+
+void setup() {
+  // put your setup code here, to run once:
+  void setSpeed(200);//0-255
+  void begin(uint16_t freq = 1600);
+  motor
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  void run(FORWARD)
+
+
+}
